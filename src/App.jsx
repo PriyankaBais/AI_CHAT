226b17b46fb720e8bb7a6b859c57e9cf23d66826
Priyanka Bais <priyanka0601bais@gmail.com>
2025-06-04 00:45:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+  const [question, setquestion] = useState("");
+  const [answer, setanswer] = useState("");
+
+
+  async function genrateAnswer() {
+   setanswer('loading...')
+    const response = await axios({
+      url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDneEYZQcefwbkVmTteM5iMZhPDlxckLak",
+      method: "post",
+      data: {
+        "contents": [{
+          "parts": [{ "text": question }]
+        },
+        ],
+      },
+    });
+    
+      setanswer(response['data']['candidates'][0]['content']['parts'][0]['text']);
+  } 
+  return (
+    <><h1>Chat AI</h1>
+      <textarea value={question} onChange={(e) => setquestion(e.target.value)}
+        cols="30" rows="10"></textarea>
+      <button onClick={genrateAnswer}>Genrate Answer</button>
+      <p>{answer}</p>
+    </>
+  )
+}
+
+export default App
